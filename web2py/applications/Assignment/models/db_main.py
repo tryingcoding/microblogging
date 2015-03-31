22acd61e71cb5c5cb45d07e79086fa6fdfbad7cc
ritvik <pulkitsarraf7@gmail.com>
2015-03-31 22:35:38 +0530

    initial code added

---
+++ web2py/applications/Assignment/models/db_main.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+db.define_table('posts',db.Field('first_name', 'string', length=255, required=True),db.Field('last_name', 'string', length=255, required=True),db.Field('last_name', 'string', length=255, required=True) )
